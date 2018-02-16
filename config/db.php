c88e039f7b4a7d9881f39821aa8805d6fdc43028
Dadan Satria <dadansatria7@gmail.com>
2018-02-17 00:00:50 +0700

    migration chat

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=realtime-chat',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
---
+++ migrations/m180216_165222_chat.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+use yii\db\Schema;
+
+/**
+ * Class m180216_165222_chat
+ */
+class m180216_165222_chat extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+/*    public function safeUp()
+    {
+
+    }*/
+
+    /**
+     * @inheritdoc
+     */
+/*    public function safeDown()
+    {
+        echo "m180216_165222_chat cannot be reverted.\n";
+
+        return false;
+    }*/
+
+    
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+        $this->createTable('chat', [
+            'id' => Schema::TYPE_PK,
+            'id_user' => Schema::TYPE_STRING . ' NOT NULL',
+            'teks' => Schema::TYPE_TEXT,
+            'waktu_dibuat' => Schema::TYPE_DATETIME,
+        ]);
+    }
+
+    public function down()
+    {
+        echo "m180216_165222_chat cannot be reverted.\n";
+
+        return false;
+    }
+    
+}
