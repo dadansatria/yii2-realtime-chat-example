41e6a89cf0ff361a54c562add5fb8287e521f26c
Dadan Satria <dadansatria7@gmail.com>
2018-10-16 21:50:27 +0700

    change database name

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=realtime-chat',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=chat',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
